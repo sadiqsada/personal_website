bc5812a7a5ade68bb2fd2e418baed42e0df1f41d
Sadiq <samin.sada@gmail.com>
2021-02-14 03:00:27 -0500

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,41 @@
 import React from 'react';
 import NextLink from 'next/link';
-import { Flex, Link, Text } from '@chakra-ui/react';
+import { Box, Flex, Heading, Link, Text } from '@chakra-ui/react';
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
   const body = (
     <>
-      <NextLink href='/'>
-        <Link ml='4vw' color='white'>
-          <Text fontSize='2xl'>Md Sadiq Sada</Text>
-        </Link>
-      </NextLink>
       <NextLink href='/projects'>
-        <Link color='white' mr={2} ml='auto'>
-          <Text fontSize='2xl'>Projects</Text>
+        <Link mr={2} ml='auto' color='#CAD5E2'>
+          Projects
         </Link>
       </NextLink>
       <a href='/resume.pdf'>
-        <Link color='white' mr={2} ml='auto'>
-          <Text fontSize='2xl'>Resume</Text>
-        </Link>
+        <Link color='#CAD5E2'>Resume</Link>
       </a>
     </>
   );
   return (
-    <Flex zIndex={1} position='sticky' top={0} p={4}>
-      {body}
+    <Flex
+      zIndex={1}
+      position='sticky'
+      top={0}
+      bg='#0D0D0D'
+      p={4}
+      align='center'
+    >
+      <Flex flex={1} margin='auto' maxW={800} align='center'>
+        <NextLink href='/'>
+          <Link>
+            <Heading size='md' color='#CAD5E2'>
+              Md Sadiq Sada
+            </Heading>
+          </Link>
+        </NextLink>
+        <Box ml='auto'>{body}</Box>
+      </Flex>
     </Flex>
   );
 };
