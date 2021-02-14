11abe7c53ee3265dc06da9b06e4364dfb8d45213
Sadiq <samin.sada@gmail.com>
2021-02-14 04:02:22 -0500

    home page in progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,39 @@
-import { Box, Stack, Text } from '@chakra-ui/react';
-import React from 'react';
+import { Box, Flex, Stack, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import { Navbar } from '../components/Navbar';
 import { Tech } from '../components/Tech';
 
 const Index = () => {
+  const statuses = ['student', 'web-developer', 'mobile-developer'];
+  const [currentState, setCurrentState] = useState(0);
+
+  useEffect(() => {
+    const intervalID = setTimeout(() => {
+      setCurrentState((currentState + 1) % 3);
+    }, 1000);
+
+    return () => clearInterval(intervalID);
+  }, []);
+
   return (
     <>
-      <Box w='100%' h='100vh' bgColor='#03203C'>
+      <Box w='100%' h='100vh' bgColor='#CAD5E2'>
         <Navbar />
         <Text
-          bgGradient='linear(to-r,gray.300,yellow.400,pink.200)'
-          bgClip='text'
-          fontSize='6xl'
-          ml='5vw'
+          align='center'
+          color='#0D0D0D'
+          fontSize='5xl'
           fontWeight='extrabold'
         >
-          Hello!
+          Hello 👋
         </Text>
-        <Text
-          bgGradient='linear(to-r,gray.300,yellow.400,pink.200)'
-          bgClip='text'
-          ml='5vw'
-          mr='5vw'
-          fontSize='3xl'
-          fontWeight='bold'
-        >
-          I'm Sadiq, a computer science, and applied mathematics junior at Stony
-          Brook University. I love to work on web applications, and I always
-          stay up to date on the latest technologies. I'm also interested in
-          other areas of computer science like machine learning. I'm currently
-          looking for an opportunity for Summer 2021. I would love to hear from
-          you!
-        </Text>
-        <br />
-        <Text
-          bgGradient='linear(to-r,gray.300,yellow.400,pink.200)'
+        <Flex align='center' maxW={800} margin='auto' justify='center'>
+          <Text color='#0D0D0D' fontSize='3xl' align='center' fontWeight='bold'>
+            I'm Sadiq, a <span>{statuses[currentState]}</span>
+          </Text>
+        </Flex>
+        {/* <Text
+          bgGradient='#0D0D0D'
           bgClip='text'
           ml='5vw'
           mr='5vw'
@@ -43,8 +42,8 @@ const Index = () => {
         >
           Technologies I Love
         </Text>
-        <br />
-        <Stack ml='5vw' spacing={8} direction='row'>
+        <br /> */}
+        {/* <Stack ml='5vw' spacing={8} direction='row'>
           <Tech imageUrl='/images/java.svg' alt='java' />
           <Tech imageUrl='/images/react.png' alt='react' />
           <Tech imageUrl='/images/node4.png' alt='node' />
@@ -52,7 +51,7 @@ const Index = () => {
           <Tech imageUrl='/images/typescript.png' alt='typescript' />
           <Tech imageUrl='/images/mongodb.png' alt='mongodb' />
           <Box mr='5vw'></Box>
-        </Stack>
+        </Stack> */}
       </Box>
     </>
   );
